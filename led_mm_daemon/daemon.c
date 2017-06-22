fb86f4ac85d4ec72fdd3d5968e178f6cab938aaf
John Lee <qinka@live.com>
2017-06-22 15:42:10 +0000

    update

--- led_mm_daemon/daemon.c
+++ led_mm_daemon/daemon.c
@@ -76,7 +76,9 @@ int main(int argc, char **argv) {
   // controller
   pin_setup(pins);
   fd = open(argv[1],O_RDWR);
-  char *buffer = mmap(NULL,1024,PROT_READ,MAP_SHARED,fd,0);
+  printf("123 %d\n",fd);
+  char *buffer = mmap(NULL,1024,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+  printf("321 %ul", buffer);
   if(buffer == MAP_FAILED) {
     perror("mmap");
     exit(1);
